b8733b55dea032c093d9cda1695b0fd45148b662
u004ff <u004ff@gmail.com>
2018-07-12 14:04:49 +0900

    test

--- app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
